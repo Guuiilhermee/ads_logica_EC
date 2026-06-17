27d31a3ea5572f83483be667558fd81540a2155e
Guilherme <guiilherme.ambrosio@gmail.com>
2026-06-16 21:49:18 -0300

    tri

---
+++ src/exercicios_Aula13/04_triangulo.js
@@ -0,0 +1,6 @@
+import leia from 'readline-sync'
+
+var lado1 = leia.questionFloat("Digite o valor do lado 1: ")
+var lado2 = leia.questionFloat("Digite o valor do lado 2: ")
+var lado3 = leia.questionFloat("Digite o valor do lado 3: ")
+
