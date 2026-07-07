603398c428d5808e8b513ceae44616b0079c12c8
Guilherme <guiilherme.ambrosio@gmail.com>
2026-07-06 21:48:04 -0300

    exercicios11

--- src/estrutura_repeticao/11_exercicio.js
+++ src/estrutura_repeticao/11_exercicio.js
@@ -1,13 +1,52 @@
 import leia from 'readline-sync';
 
-var pessoas = 0
-var masc = 0
-var fem = 1
+var pessoas = 0;
 
-while(pessoas <= 10){
-    var genero = leia.keyInSelect(["F", "M"],
-    "INFORME O GENERO"
-)
-    var altura = leia.questionFloat("DIGITE SUA ALTURA: ")
+var maiorAltura = 0;
+var menorAltura = 0;
 
+var somaAlturas = 0;
+
+var somaMulheres = 0;
+var qtdMulheres = 0;
+
+var qtdHomens = 0;
+
+while (pessoas < 10) {
+
+    var genero = leia.keyInSelect(["F", "M"], "INFORME O GENERO");
+
+    var altura = leia.questionFloat("DIGITE SUA ALTURA: ");
+
+    if (altura > maiorAltura) {
+        maiorAltura = altura;
+    }
+
+    if (altura < menorAltura) {
+        menorAltura = altura;
+    }
+
+    somaAlturas += altura;
+
+    if (genero === 0) {
+        somaMulheres += altura;
+        qtdMulheres++;
+    }
+
+    if (genero === 1) {
+        qtdHomens++;
+    }
+
+    pessoas++;
 }
+
+var mediaMulheres = qtdMulheres > 0 ? somaMulheres / qtdMulheres : 0;
+var mediaPopulacao = somaAlturas / 10;
+var percentualHomens = (qtdHomens / 10) * 100;
+
+console.log("\nRESULTADOS");
+console.log(`Maior altura: ${maiorAltura.toFixed(2)} m`);
+console.log(`Menor altura: ${menorAltura.toFixed(2)} m`);
+console.log(`Média das mulheres: ${mediaMulheres.toFixed(2)} m`);
+console.log(`Média da população: ${mediaPopulacao.toFixed(2)} m`);
+console.log(`Percentual de homens: ${percentualHomens}%`);
