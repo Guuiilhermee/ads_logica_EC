5ee6963e080de0a118781ab99291144970f6e1f4
Guilherme <guiilherme.ambrosio@gmail.com>
2026-07-20 21:49:59 -0300

    inicioarray

---
+++ src/estrutura_dados/explicacao_02.js
@@ -0,0 +1,21 @@
+var listaAlunos = ["Guilherme", "Gustavo"]
+var notas = [7, 8.8]
+
+var index = 0
+var soma = notas[index] + notas[1]
+
+listaAlunos[0] = "Joaninha"
+listaAlunos = "Aninha"
+
+listaAlunos.push("Pedro")
+listaAlunos.unshift("Carlos")
+console.log(listaAlunos)
+
+listaAlunos.pop()
+listaAlunos.shift()
+
+listaAlunos.length
+
+console.log(listaAlunos)
+console.log(listaAlunos[0])
+console.log(listaAlunos[1])
