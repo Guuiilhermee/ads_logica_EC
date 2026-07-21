a4d20f705c8fe580ee50f5d3d0568c7babf37a53
Guilherme <guiilherme.ambrosio@gmail.com>
2026-07-21 19:13:38 -0300

    exercicios

--- src/estrutura_dados/exercicios/03_exercicio.js
+++ src/estrutura_dados/exercicios/03_exercicio.js
@@ -1,21 +1,23 @@
 import leia from 'readline-sync';
 
-var numeros = [];
-var pares = [];
-
-console.log("Digite 10 números inteiros:");
-
-for (var i = 0; i < 10; i++) {
-    var num = leia.questionInt(`Digite o ${i + 1}º número: `);
-    numeros.push(num);
-    if (num % 2 === 0) pares.push(num);
-}
-
-console.log("\nVetor completo:", numeros);
-console.log("Números pares:", pares);
-console.log("Quantidade de pares:", pares.length);
-
-console.log("\nLista de números pares:");
-for (var i = 0; i < pares.length; i++) {
-    console.log(pares[i]);
+export function exercicio03(){
+    var numeros = [];
+    var pares = [];
+    
+    console.log("Digite 10 números inteiros:");
+    
+    for (var i = 0; i < 10; i++) {
+        var num = leia.questionInt(`Digite o ${i + 1} numero: `);
+        numeros.push(num);
+        if (num % 2 === 0) pares.push(num);
+    }
+    
+    console.log("\nVetor completo:", numeros);
+    console.log("Números pares:", pares);
+    console.log("Quantidade de pares:", pares.length);
+    
+    console.log("\nLista de números pares:");
+    for (var i = 0; i < pares.length; i++) {
+        console.log(pares[i]);
+    }
 }
